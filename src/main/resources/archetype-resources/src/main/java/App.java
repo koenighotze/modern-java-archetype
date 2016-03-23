e1d41b6f0f8595ba5c8ccc417ca05a5ed7e42b0f
David Schmitz <david.schmitz.privat@gmail.com>
2016-03-23 08:54:52 +0100

    import

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package $org.koenighotze;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,17 @@
+package $org.koenighotze;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void smoke() {
+        assertTrue(true);
+    }
+}
